2b417a218f18ad03f9ee5a3c7601ab7179b3dd58
LufeSS <A01742322@itesm.mx>
2021-08-21 18:57:58 +0000

    02 y 04

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+    x = int(input("Ingresa el primer número: "))
+    y = int(input("Ingresa el segundo número: "))
+    z = int(input("Ingresa el tercer número: "))
+
+    if x >= y and x >= z:
+        print(x)
+    elif y >= x and y >= z:
+        print(y)
+    elif z >= x and z >= y:
+        print(z)
 
 if __name__=='__main__':
     main()
